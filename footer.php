f749a1aa42f6e550982ae09f0b4665d0e09eedaa
Jacky <jackyhau97@gmail.com>
2021-03-23 14:59:06 +0700

    Customizing Footer Component.

--- footer.php
+++ footer.php
@@ -2,27 +2,31 @@
   <div class="container-fluid container-footer">
     <div class="row d-flex align-items-center p-4">      
       <div class="col-lg-12">
-        <div class="row">
-          <div class="col-12 col-lg-4 text-center">
+        <div class="row d-flex align-items-center">
+          <div class="col-12 col-lg-4 text-center container-dealer">
             <a href="" class="mmc-bold">DEALER RESMI MITSUBISHI MEDAN</a><br>
             <a href="https://www.sardanagroup.co.id" target="_blank"><i class="fas fa-building"></i> PT. Sardana IndahBerlian Motor</a><br>
             <a class="m-0 p-0 white" href="https://www.google.com/maps/place/Mitsubishi+Sardana+Medan+-+Gatot+Subroto/@3.5897229,98.6548474,17z/data=!3m1!4b1!4m5!3m4!1s0x30312f94a86ecdbb:0x83c7b521f607d461!8m2!3d3.5897229!4d98.6570361?hl=en" target="_blank"><i class="fas fa-map-marker-alt"></i> Jl. Jend. Gatot Subroto No.437, Medan, Sumatera Utara - 20119</a><br>            
-            <a href="https://wa.me/+628116541800?text=Saya ingin bertanya tentang produk Mitsubishi" target="_blank"><i class="fab fa-lg fa-whatsapp"></i> WA: <b>0823 7030 4974</b></a><br />
+            <a href="https://wa.me/+6282370304974?text=Saya ingin bertanya tentang produk Mitsubishi" target="_blank"><i class="fab fa-lg fa-whatsapp"></i> WA: <b>0823 7030 4974</b></a><br />
           </div>       
-          <div class="col-12 col-md-6 col-lg-4" style="margin: auto">
-            <h3 class="text-uppercase tetap">tetap terhubung dengan kami</h3>
+          <div class="col-12 col-md-6 col-lg-4 tetap container-tetap">
+            <h7 class="text-uppercase">tetap terhubung dengan kami</h7>
             <ul>
-              <li> <a href="https://wa.me/+628116541800?text=Saya ingin bertanya tentang produk Mitsubishi" target="_blank"><i class="fab fa-lg fa-whatsapp"></i> WA: <b>0823 7030 4974</b></a><br /></li>
+              <li> <a href="https://wa.me/+6282370304974?text=Saya ingin bertanya tentang produk Mitsubishi" target="_blank"><i class="fab fa-lg fa-whatsapp"></i></a></li>
               <li><a href="https://www.facebook.com/sardanaindahberlianmotor" target="_blank"><i class="fab fa-lg fa-facebook-f"></i></a></li>                
               <li><a href="https://www.instagram.com/vandys_mitsubishi" target="_blank"><i class="fa fa-lg fa-instagram" aria-hidden="true"></i></a></li>              
             </ul> 
           </div>
+          <div class="col-12 col-md-6 col-lg-4 text-center">
+            <label class="col-12 mmc-bold">Alamat Kami</label>        
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.9901973063243!2d98.654847414108!3d3.5897228973877535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30312f94a86ecdbb%3A0x83c7b521f607d461!2sMitsubishi%20Sardana%20Medan%20-%20Gatot%20Subroto!5e0!3m2!1sen!2sid!4v1611027364878!5m2!1sen!2sid" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0" class="alamat"></iframe>
+          </div>
         </div>
       </div>      
     </div>   
-    <div class="row p-3 copyright-container">
-      <div class="col-12 d-flex justify-content-center">
-        <p class="copyright">Copyright &copy; <?= date('Y'); ?> PT. Sardana IndahBerlianMotor | Mitsubishi Motors Authorized Dealer in Medan</p>
+    <div class="row">
+      <div class="col-12">
+        <p class="text-right">Copyright &copy; <?= date('Y'); ?> by JaVa</p>
       </div>
     </div> 
   </div>
